dc6fcbfe644d3b1ef179f330c1bb0d231a5574f3
Luilisia <Lh029@hdm-stuttgart.de>
2013-11-24 20:10:51 +0100

    ...

--- IT-Projekt/src/de/hdm/itprojekt/shared/Verwaltungsklasse.java
+++ IT-Projekt/src/de/hdm/itprojekt/shared/Verwaltungsklasse.java
@@ -6,6 +6,8 @@ import de.hdm.itprojekt.shared.bo.*;
 
 public interface Verwaltungsklasse extends RemoteService {
 	
+	public void init() throws IllegalArgumentException;
+	
 	public Dozent createDozent(String vorname, String nachname, String email)
 		throws IllegalArgumentException;
 	
@@ -18,4 +20,24 @@ public interface Verwaltungsklasse extends RemoteService {
 	public Semesterverband createSemesterverband(String studiengang, int semester, int studierendenAnzahl)
 			throws IllegalArgumentException;
 	
+	public Dozent deleteDozent(Dozent a) throws IllegalArgumentException;
+	
+	public Lehrveranstaltung deleteLehrveranstaltung(Lehrveranstaltung a) throws IllegalArgumentException;
+	
+	public Raum deleteRaum(Raum a) throws IllegalArgumentException;
+	
+	public Semesterverband deleteSemesterverband(Semesterverband a) throws IllegalArgumentException;
+	
+	public Dozent changeDozent(String vorname, String nachname, String email)
+			throws IllegalArgumentException;
+	
+	public Lehrveranstaltung changeLehrveranstaltung(String bezeichnung, int semester, int umfang)
+			throws IllegalArgumentException;
+	
+	public Raum changeRaum(String name, int kapazitaet)
+			throws IllegalArgumentException;
+	
+	public Semesterverband changeSemsterverband(String studiengang, int semester, int studierendenAnzahl)
+			throws IllegalArgumentException;
 }
+
--- IT-Projekt/src/de/hdm/itprojekt/shared/VerwaltungsklasseAsync.java
+++ IT-Projekt/src/de/hdm/itprojekt/shared/VerwaltungsklasseAsync.java
@@ -5,6 +5,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import de.hdm.itprojekt.shared.bo.*;
 
 public interface VerwaltungsklasseAsync {
+	
+	void init(AsyncCallback<Void> callback);
 
 	void createDozent(String vorname, String nachname, String email,
 			AsyncCallback<Dozent> callback);
@@ -16,5 +18,27 @@ public interface VerwaltungsklasseAsync {
 	
 	void createSemesterverband(String studiengang, int semester,
 			int studierendenAnzahl, AsyncCallback<Semesterverband> callback);
+
+	void deleteDozent(Dozent a, AsyncCallback<Dozent> callback);
+
+	void deleteLehrveranstaltung(Lehrveranstaltung a,
+			AsyncCallback<Lehrveranstaltung> callback);
+
+	void deleteRaum(Raum a, AsyncCallback<Raum> callback);
+
+	void deleteSemesterverband(Semesterverband a,
+			AsyncCallback<Semesterverband> callback);
+
+	void changeDozent(String vorname, String nachname, String email,
+			AsyncCallback<Dozent> callback);
+
+	void changeLehrveranstaltung(String bezeichnung, int semester, int umfang,
+			AsyncCallback<Lehrveranstaltung> callback);
+
+	void changeRaum(String name, int kapazitaet, AsyncCallback<Raum> callback);
+
+	void changeSemsterverband(String studiengang, int semester,
+			int studierendenAnzahl, AsyncCallback<Semesterverband> callback);
+
 
 }
