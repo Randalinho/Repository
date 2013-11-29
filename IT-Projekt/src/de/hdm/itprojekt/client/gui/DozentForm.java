72bb155b7926855ec9287d10a078c0d7bcdcb2c6
Verena92 <vh031@hdm-stuttgart.de>
2013-11-29 15:52:00 +0100

    Verena

---
+++ IT-Projekt/src/de/hdm/itprojekt/client/gui/DozentForm.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.hdm.itprojekt.client.gui;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * @author Verena Hofmann
+ *
+ */
+public class DozentForm extends VerticalPanel{
+
+	/**
+	 * 
+	 */
+	public DozentForm() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
