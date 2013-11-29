637fad3915faced651efd5af84fb38911238be0d
Verena92 <vh031@hdm-stuttgart.de>
2013-11-29 16:08:00 +0100

    Verena

---
+++ IT-Projekt/src/de/hdm/itprojekt/client/gui/DozentForm.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package de.hdm.itprojekt.client.gui;
+
+/**
+ * @author Verena Hofmann
+ *
+ */
+public class DozentForm {
+
+	/**
+	 * 
+	 */
+	public DozentForm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
